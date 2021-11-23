bb2c47b5564dbd26af9c463e670fd25eb8cd69af
hainguyen201 <55351779+hainguyen201@users.noreply.github.com>
2021-11-23 14:09:47 +0700

    Update Upload.js

--- Upload.js
+++ Upload.js
@@ -1,4 +1,46 @@
-postData(){
+<template>
+  <div class="addform">
+    <div>
+      <span>Họ tên</span>
+      <input type="text" v-model="emploeeName"/>
+    </div>
+    <div>
+      <span>Ngày sinh</span>
+      <DatePicker style="display: inline-block" v-model="employeeDateOfBirth"></DatePicker>
+    </div>
+    <div>
+      <span>Địa chỉ</span>
+      <input type="text" v-model="employeeAddress"/>
+    </div>
+    <div><input type="file" accept="image/jpeg" ref="avatarImage"></div>
+    <button class="btn btn-dark" v-on:click='postData()'>Thêm</button>
+    <button class="btn btn-dark" v-on:click='getData()'>Get</button>
+    <div ref="listImage" v-for="avatar in avatars" :key="avatar.id">
+      <img :src='avatar'>
+    </div>
+  </div>
+</template>
+<script>
+import DatePicker from "vuejs-datepicker";
+import moment from 'moment';
+import {mapActions} from "vuex";
+export default {
+  name: "AddEmployeeForm",
+  data() {
+      return {
+          emploeeName:'',
+          employeeAddress:'',
+          employeeDateOfBirth:null,
+          employee:{},
+          avatars:[],
+      }
+  },
+  components: {
+    DatePicker
+  },
+  methods:{
+      ...mapActions(['postEmployee', 'fetchEmployee']),
+      postData(){
           var employee={};
           employee.employeeDateOfBirth=this.dataFormat(this.employeeDateOfBirth);
           employee.employeeName=this.emploeeName;
@@ -12,8 +54,41 @@ postData(){
           reader.onload= (e)=>{
             fd.append('employee', JSON.stringify(employee));
             fd.append('file', e.target.result)
+            debugger
             this.postEmployee(fd);
           }
 
 
       },
+      getData(){
+        this.avatars=[]
+        this.fetchEmployee().then(data=>{
+          data.forEach(element => {
+            console.log(this.dataFormat(element.employeeDateOfBirth))
+            element.avatar=atob(element.avatar);
+            console.log(element.avatar)
+            this.avatars.push(element.avatar)
+            // this.$refs.listImage.append(`<image src="${element.avatar}">`)
+          });
+          // console.log(data)
+        })
+      },
+      dataFormat(value){
+          if(value){
+              return moment(String(value)).format('DD/MM/yyyy')
+          }else{
+            return ''
+          }
+      }
+  }
+};
+</script>
+<style  scoped>
+.addform span{
+    display: inline-block;
+    min-width: 100px;
+}
+.addform>div{
+    margin: 10px;
+}
+</style>
